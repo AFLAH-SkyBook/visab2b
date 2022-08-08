e5d92c87104e4e711186a6dca4ed66a4983d33b6
Aflah <aflahvk2527@gmail.com>
2022-08-08 16:32:42 +0530

    settings file setup

--- visamanagement/settings.py
+++ visamanagement/settings.py
@@ -5,7 +5,7 @@ import environ
 # Initialise environment variables
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=True)
+READ_DOT_ENV_FILE = env.bool('READ_DOT_ENV_FILE', default=False)
 if READ_DOT_ENV_FILE:
     environ.Env.read_env()
 
@@ -149,3 +149,18 @@ MEDIA_URL = '/media/'
   
 # Path where media is stored  
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    X_FRAME_OPTIONS = "DENY"
+
+    ALLOWED_HOSTS = ["*"]
